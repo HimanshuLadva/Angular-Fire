6dc6e45aae5211446e36fd649db34a83aa945a84
HimanshuLadva <himanshuladva2241@gmail.com>
2023-06-30 19:01:42 +0530

    Complete till starting emulators

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  useEmulators: true,
   firebase: {
     apiKey: "AIzaSyBxjrCv1HrfMcfhqvHqE5Ab5k3Kf0nt0mM",
     authDomain: "angularfire-course-8e149.firebaseapp.com",
